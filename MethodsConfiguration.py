c79b3793fc76eb69d358342724016ff2fa085fe3
pawelpeksa <pmpeksa@gmail.com>
2017-07-08 19:08:58 +0200

    add first code

---
+++ MethodsConfiguration.py
@@ -0,0 +1,23 @@
+
+class MethodsConfiguration:
+	def __init__(self):
+		pass
+
+	class SVM:
+		def __init__(self):
+			C = 0
+
+	class ANN:
+		def __init__(self):
+			hidden_neurons = 0
+			solver = ""
+			alpha = 0
+	
+	class RandomForest:
+		def __init__(self):
+			max_depth = 0
+			n_estimators = 0			
+
+	class DecisionTree:
+		def __init__(self):
+			max_depth = 0			
