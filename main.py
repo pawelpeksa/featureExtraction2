17975659709a7778f032f16dfc6bbda0c20bb442
pawelpeksa <pmpeksa@gmail.com>
2017-07-09 16:15:11 +0200

    refactor

--- main.py
+++ main.py
@@ -38,18 +38,33 @@ def main():
     x = digits.data
     y = digits.target
 
-    dimenstions = x.shape[1]
-
     x, y = prepare_dataset(x, y)
 
-    # split data for train and test
+    calculate(x, y)
 
-    config = determine_parameters_all(x, y)
+
+def calculate(x, y):
+    dimenstions = x.shape[1]
+
+    # hold out for test 
+    x_train, x_test, y_train, y_test = train_test_split(x, y, test_size=200, random_state=int(time.time()))
+
+    # calculate for different train data size
+    for train_data_size in range(300, 1501, 300):
+        # we don't need tmp1 and tmp2 because test set was extracted before
+        x_train, tmp1, y_train, tmp2 = train_test_split(x, y, train_size=train_data_size, random_state=int(time.time()))
+
+        assert(x_train.shape[0] == train_data_size)
     
-    result_file_prefix = 'digits'
+        config = determine_parameters_all(x_train, y_train)
 
-    test_data_set(x_train, y_train, x_test, y_test, result_file_prefix, dimenstions, config)
+        suffix = str(train_data_size)
 
+        save_methods_config(config, 'methods_config_' + suffix + 'dat')
+
+        result_file_prefix = 'digits_' + suffix
+
+        test_data_set(x_train, y_train, x_test, y_test, result_file_prefix, dimenstions, config)
 
 def prepare_dataset(x, y):
     # get 1700 out of 1797 samples
@@ -66,15 +81,13 @@ def determine_parameters_all(x_train, y_train):
 
     config.svm;C = 1
     config.ann.hidden_neurons, config.ann.solver, config.ann.alpha = 15, 'adam', 0.5
-    config.decision_tree.max_depth = '5'
+    config.decision_tree.max_depth = 5
     config.random_forest.max_depth, config.random_forest.n_estimators = 5, 5
-
-    save_methods_config(config)
 
     return config
 
-def save_methods_config(config):
-    with open('methods_config.dat', 'w') as output:
+def save_methods_config(config, file_name):
+    with open(file_name, 'w') as output:
         json.dump(config.toDict(), output)    
 
 
@@ -98,17 +111,17 @@ def test_given_extraction_method(x_train, y_train, x_test, y_test, reduction_obj
 
     x_train, x_test = reduce_dimensions(x_train, y_train, x_test, y_test, reduction_object)
 
-    print "Components:", reduction_object.n_components, "\n"
+    print "Components:", reduction_object.n_components, ' ', file_prefix, '\n'
 
     SVM = svm.SVC(kernel='linear', C=1).fit(x_train, y_train)
     score_1 = SVM.score(x_test, y_test)
 
     print "svm", score_1, "\n"
 
-    ann = MLPClassifier(solver=config.ANN.solver, 
+    ann = MLPClassifier(solver=config.ann.solver, 
                         max_iter=ANN_MAX_ITER, 
-                        alpha=config.ANN.alpha, 
-                        hidden_layer_sizes=(config.ANN.hidden_neurons,), 
+                        alpha=config.ann.alpha, 
+                        hidden_layer_sizes=(config.ann.hidden_neurons,), 
                         random_state=1, 
                         learning_rate='adaptive')
 
@@ -117,11 +130,11 @@ def test_given_extraction_method(x_train, y_train, x_test, y_test, reduction_obj
     score_2 = ann.score(x_test, y_test)
     print "ann", score_2, "\n"
 
-    forest = RandomForestClassifier(max_depth=5, n_estimators=10).fit(x_train, y_train)
+    forest = RandomForestClassifier(max_depth=config.random_forest.max_depth, n_estimators=config.random_forest.n_estimators).fit(x_train, y_train)
     score_3 = forest.score(x_test, y_test)
     print "random forest", score_3, "\n"
 
-    tree = DecisionTreeClassifier(max_depth=decision_tree_depth).fit(x_train, y_train)
+    tree = DecisionTreeClassifier(max_depth=config.decision_tree.max_depth).fit(x_train, y_train)
     score_4 = tree.score(x_test, y_test)
     print "decision tree", score_4, "\n"
 
