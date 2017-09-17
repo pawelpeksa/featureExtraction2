02d47ca1895db4006b83604513a835f5df1f4fc0
pawelpeksa <pmpeksa@gmail.com>
2017-09-17 23:25:50 +0200

    fix bug with using pca/lda sperately for train/test set

--- Configuration.py
+++ Configuration.py
@@ -5,6 +5,6 @@ class Configuration:
     HYPEROPT_EVALS_PER_SEARCH = 40
     ANN_MAX_ITERATIONS = 1000
     ANN_OPIMIZER_MAX_ITERATIONS = 500
-    SAMPLES_N = [500, 1000]
-    DIMS = [1, 10, 20, 30, 40, 50, 60, 70, 80, 90, 100]
-    MAX_FEATURES = 100
+    MAX_FEATURES = 64
+    SAMPLES_N = [1000]
+    DIMS = range(1, 65) # digits dataset has 64 dims
